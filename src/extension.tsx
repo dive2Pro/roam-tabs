58844299d335c04e11ddad03e07bf1e36c98112b
toyluck <toylucklebo@gmail.com>
2023-08-10 10:50:59 +0800

    feat: context menu to close

--- src/extension.tsx
+++ src/extension.tsx
@@ -1,31 +1,46 @@
-import React, { FC, useReducer } from "react";
+import React, { Component, FC, useReducer } from "react";
 import ReactDOM from "react-dom";
 import "./style.less";
 const { useEffect, useState, useCallback, useRef, useLayoutEffect } = React;
-import { Button, Icon, MenuItem, Menu } from '@blueprintjs/core'
+import {
+  Button,
+  Icon,
+  MenuItem,
+  Menu,
+  ContextMenuTarget,
+  ContextMenu,
+} from "@blueprintjs/core";
 import { extension_helper } from "./helper";
-import { isAutoOpenNewTab, loadTabsFromSettings, saveTabsToSettings } from "./config";
+import {
+  isAutoOpenNewTab,
+  loadTabsFromSettings,
+  saveTabsToSettings,
+} from "./config";
 import { Omnibar } from "@blueprintjs/select";
-import { NodeGroup } from 'react-move'
+import { NodeGroup } from "react-move";
 
 const clazz = "roam-tabs";
 let scrollTop$ = 0;
 
-const delay = (ms = 10) => new Promise((resolve) => setTimeout(() => { resolve(1) }, ms));
-
+const delay = (ms = 10) =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(1);
+    }, ms)
+  );
 
 function debounce(fn: Function, ms = 500) {
-  let timer = setTimeout(() => { }, 0)
+  let timer = setTimeout(() => {}, 0);
   return (...args: any[]) => {
     clearTimeout(timer);
     timer = setTimeout(() => {
-      fn(...args)
-    }, ms)
-  }
+      fn(...args);
+    }, ms);
+  };
 }
 
 const _mount = async () => {
-  console.log(' mounting :::')
+  console.log(" mounting :::");
   const roamMain = document.querySelector(".roam-main");
   let el = roamMain.querySelector("." + clazz);
   const roamBodyMain = roamMain.querySelector(".roam-body-main");
@@ -36,20 +51,22 @@ const _mount = async () => {
     roamMain.insertBefore(el, roamBodyMain);
     ReactDOM.render(<App />, el);
   } else {
-    forceUpdate()
+    forceUpdate();
   }
   saveTabsToSettings(tabs, currentTab);
 
   // scroll to active button
   setTimeout(() => {
     const rbm = document.querySelector(".rm-article-wrapper");
-    rbm.scrollTop = currentTab?.scrollTop || 0
+    rbm.scrollTop = currentTab?.scrollTop || 0;
 
     function scrollIntoActiveTab() {
-      const activeEl = document.querySelector(".roam-tab-active") as HTMLElement;
+      const activeEl = document.querySelector(
+        ".roam-tab-active"
+      ) as HTMLElement;
       const parentEl = activeEl?.parentElement;
       if (!activeEl || !parentEl) {
-        return
+        return;
       }
 
       const childRect = activeEl.getBoundingClientRect();
@@ -67,21 +84,20 @@ const _mount = async () => {
 
       if (scrollLeft < itemLeft) {
         parentEl.scroll({
-          left: itemLeft
-        })
+          left: itemLeft,
+        });
       } else if (scrollLeft + parentRect.width < itemLeft) {
         parentEl.scroll({
-          left: itemLeft
-        })
+          left: itemLeft,
+        });
       } else if (scrollLeft + parentRect.width > itemRight) {
         parentEl.scroll({
-          left: itemRight
-        })
+          left: itemRight,
+        });
       }
     }
     scrollIntoActiveTab();
-
-  }, 100)
+  }, 100);
 };
 
 const mount = debounce(_mount, 150);
@@ -109,7 +125,7 @@ const setTabs = (newTab: Tab) => {
     }
     if (!currentTab) {
       prev[0] = newTab;
-      setCurrentTab(newTab)
+      setCurrentTab(newTab);
       return [...prev];
     }
     const i = prev.findIndex((tab) => currentTab.uid === tab.uid);
@@ -130,24 +146,39 @@ const removeTab = (uid: string) => {
   mount();
 };
 
+const removeOtherTbas = (lastTab: Tab) => {
+  tabs = [lastTab];
+  setCurrentTab(lastTab);
+  mount();
+};
+
+const removeToTheRightTabs = (index: number) => {
+  tabs = tabs.slice(0, index + 1);
+  const currentIndex = tabs.findIndex((t) => t.uid === currentTab?.uid);
+  if (currentIndex === -1 || currentIndex > index) {
+    setCurrentTab(tabs[index]);
+  }
+  mount();
+};
+
 const setCurrentTab = (v?: Tab) => {
   if (v) {
     const oldTab = tabs.find((tab) => tab.uid === v.uid);
     currentTab = {
       ...oldTab,
-      ...v
-    }
+      ...v,
+    };
   } else {
     currentTab = v;
   }
-  console.log(tabs, currentTab, ' setCurrentTab')
+  console.log(tabs, currentTab, " setCurrentTab");
   v && openUid(v.blockUid);
 };
 
 let routeChanging = false;
-let forceUpdate = () => { };
+let forceUpdate = () => {};
 function App() {
-  forceUpdate = useReducer(i => i + 1, 0)[1]
+  forceUpdate = useReducer((i) => i + 1, 0)[1];
   const onChange = useEvent((uid: string, title: string, blockUid: string) => {
     if (uid) {
       const oldTab = tabs.find((tab) => tab.uid === uid);
@@ -156,43 +187,39 @@ function App() {
         ...oldTab,
         uid,
         title,
-        blockUid
+        blockUid,
       };
-      setTabs(newTab)
+      setTabs(newTab);
       setCurrentTab(newTab);
     } else {
-      setCurrentTab()
+      setCurrentTab();
     }
     mount();
   });
   const onPointerdown = useEvent(function onPointerdown(e: PointerEvent) {
-
     ctrlKeyPressed = e.ctrlKey || e.metaKey;
-    const rbm = document.querySelector(".rm-article-wrapper")
+    const rbm = document.querySelector(".rm-article-wrapper");
     // console.log(scrollTop$, ' ---global')
   });
 
-
-
   useEffect(() => {
-    const onScroll = ((e: Event) => {
-      const rbm = document.querySelector(".rm-article-wrapper")
+    const onScroll = (e: Event) => {
+      const rbm = document.querySelector(".rm-article-wrapper");
 
-      scrollTop$ = rbm.scrollTop
-      console.log('onScroll ---, ', routeChanging)
-      if (!routeChanging)
-        recordPosition();
-    })
-    const rbm = document.querySelector(".rm-article-wrapper")
+      scrollTop$ = rbm.scrollTop;
+      console.log("onScroll ---, ", routeChanging);
+      if (!routeChanging) recordPosition();
+    };
+    const rbm = document.querySelector(".rm-article-wrapper");
     if (!rbm) {
-      return
+      return;
     }
 
-    rbm.addEventListener("scroll", onScroll)
+    rbm.addEventListener("scroll", onScroll);
     return () => {
-      rbm.removeEventListener("scroll", onScroll)
-    }
-  }, [])
+      rbm.removeEventListener("scroll", onScroll);
+    };
+  }, []);
   useEffect(() => {
     const onRouteChange = async (e: HashChangeEvent) => {
       routeChanging = true;
@@ -205,7 +232,7 @@ function App() {
 
       if (index === -1) {
         setCurrentTab();
-        mount()
+        mount();
         routeChanging = false;
         return;
       }
@@ -214,12 +241,11 @@ function App() {
       console.log("change: true ", pageUid, title, uid, tabs);
       onChange(pageUid, title, uid);
       routeChanging = false;
-
-    }
-    window.addEventListener("hashchange", onRouteChange)
+    };
+    window.addEventListener("hashchange", onRouteChange);
 
     return () => {
-      window.removeEventListener("hashchange", onRouteChange)
+      window.removeEventListener("hashchange", onRouteChange);
     };
   }, []);
 
@@ -230,148 +256,211 @@ function App() {
     };
   }, []);
 
-  console.log('render: ', currentTab)
+  console.log("render: ", currentTab);
   return (
     <div className="roam-tabs-container">
-      {tabs.map((tab) => {
+      {tabs.map((tab, index) => {
         const active = tab.uid === currentTab?.uid;
-        return (
-          <Button
-            style={{
-              outline: 'none'
-            }}
-            intent={active ? "primary" : "none"}
-            outlined
-            small
-            className={`${active ? "roam-tab-active" : ''} roam-tab`}
-            onClick={(e) => {
-              console.log(' button - click ', JSON.stringify(tab))
-              if (e.shiftKey) {
-                openInSidebar(tab.uid);
-                return;
-              }
-              setCurrentTab(tab);
-              mount()
-            }}
-            rightIcon={
-              <Button
-                minimal
-                small
-                icon={<Icon color="#ABB3BF" icon="small-cross" />}
-                onClickCapture={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  removeTab(tab.uid);
-                  mount();
-                }}
-              />
-            }
-            text={
-              <div
-                style={{
-                  display: "inline-block",
-
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  minWidth: 100,
-                  maxWidth: 200 /* 设置最大宽度 */
-                }}
-              >
-                {tab.title}
-              </div>
-            }
-          ></Button>
-        );
+        return <AppTab active={active} index={index} tab={tab} />;
       })}
       <SwitchCommand tabs={tabs} />
     </div>
   );
 }
 
+class AppTab extends Component<{ active: boolean; tab: Tab; index: number }> {
+  renderContextMenu() {
+    return (
+      <Menu>
+        <MenuItem onClick={() => {}} text="Save" />
+        <MenuItem onClick={() => {}} text="Delete" />
+      </Menu>
+    );
+  }
 
+  render() {
+    const { active, tab, index } = this.props;
+    return (
+      <Button
+        style={{
+          outline: "none",
+        }}
+        intent={active ? "primary" : "none"}
+        outlined
+        small
+        className={`${active ? "roam-tab-active" : ""} roam-tab`}
+        onContextMenu={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          ContextMenu.show(
+            <Menu>
+              <MenuItem
+                text="Close"
+                onClick={() => {
+                  removeTab(tab.uid);
+                }}
+              />
+              <MenuItem
+                text="Close Others"
+                onClick={() => {
+                  removeOtherTbas(tab);
+                }}
+                disabled={tabs.length === 1}
+              />
+              <MenuItem
+                onClick={() => {
+                  removeToTheRightTabs(index);
+                }}
+                text="Close to the Right"
+                disabled={index + 1 >= tabs.length }
+              />
+            </Menu>,
+            { left: e.clientX, top: e.clientY },
+            () => {}
+          );
+        }}
+        onClick={(e) => {
+          if (e.shiftKey) {
+            openInSidebar(tab.uid);
+            return;
+          }
+          setCurrentTab(tab);
+          mount();
+        }}
+        rightIcon={
+          <Button
+            minimal
+            small
+            icon={<Icon color="#ABB3BF" icon="small-cross" />}
+            onClickCapture={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              removeTab(tab.uid);
+              mount();
+            }}
+          />
+        }
+        text={
+          <div
+            style={{
+              display: "inline-block",
+
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              minWidth: 100,
+              maxWidth: 200 /* 设置最大宽度 */,
+            }}
+          >
+            {tab.title}
+          </div>
+        }
+      ></Button>
+    );
+  }
+}
 
 function SwitchCommand(props: { tabs: Tab[] }) {
   const [state, setState] = useState({
     open: false,
-  })
+  });
   const [activeItem, setActiveItem] = useState<Tab>();
   useEffect(() => {
     API.ui.commandPalette.addCommand({
-      label: 'Switch Tab...',
+      label: "Switch Tab...",
       callback() {
         setState({
           open: !state.open,
-        })
+        });
       },
-    })
+    });
   }, []);
 
-  return <Omnibar
-    isOpen={state.open}
-    onClose={() => setState({ open: false })}
-    items={props.tabs}
-    itemPredicate={(query, item) => {
-      return item.title.toLowerCase().includes(query.toLowerCase())
-    }}
-    itemRenderer={(item, itemProps) => {
-      return <MenuItem
-        onClick={itemProps.handleClick}
-        {...itemProps.modifiers}
-        text={highlightText(item.title, itemProps.query)}
-
-      />
-    }}
-    onItemSelect={(item) => {
-      console.log(item, ' select ')
-      setCurrentTab(item)
-      setState({ open: false })
-      mount();
-    }}
-    itemListRenderer={(itemListProps) => {
-      return <Menu>
-        <NodeGroup
-          data={itemListProps.filteredItems}
-          start={(data, index) => {
-            return {
-              y: 35,
-              opacity: 0
-            }
-          }}
-          enter={(data, index) => {
-            return {
-              opacity: [1],
-              timing: {
-                duration: 250
-              }
-            }
-          }}
-          leave={(data, index) => {
-            return [{
-              y: [-35],
-              timing: {
-                duration: 250
-              }
-            }, {
-              opacity: [0],
-              timing: {
-                duration: 150
-              }
-
-            }]
-          }}
-          keyAccessor={(data) => data.uid}
-        >
-          {(nodes) => {
-            return <>{nodes.map((node, index) => {
-              return <div key={node.key} style={{ opacity: node.state.opacity, height: node.state.y }}>{itemListProps.renderItem(node.data, index)}</div>
-            })}
-            </>
-          }}
-        </NodeGroup>
-      </Menu>
-    }}
-  />
+  return (
+    <Omnibar
+      isOpen={state.open}
+      onClose={() => setState({ open: false })}
+      items={props.tabs}
+      itemPredicate={(query, item) => {
+        return item.title.toLowerCase().includes(query.toLowerCase());
+      }}
+      itemRenderer={(item, itemProps) => {
+        return (
+          <MenuItem
+            onClick={itemProps.handleClick}
+            {...itemProps.modifiers}
+            text={highlightText(item.title, itemProps.query)}
+          />
+        );
+      }}
+      onItemSelect={(item) => {
+        console.log(item, " select ");
+        setCurrentTab(item);
+        setState({ open: false });
+        mount();
+      }}
+      itemListRenderer={(itemListProps) => {
+        return (
+          <Menu>
+            <NodeGroup
+              data={itemListProps.filteredItems}
+              start={(data, index) => {
+                return {
+                  y: 35,
+                  opacity: 0,
+                };
+              }}
+              enter={(data, index) => {
+                return {
+                  opacity: [1],
+                  timing: {
+                    duration: 250,
+                  },
+                };
+              }}
+              leave={(data, index) => {
+                return [
+                  {
+                    y: [-35],
+                    timing: {
+                      duration: 250,
+                    },
+                  },
+                  {
+                    opacity: [0],
+                    timing: {
+                      duration: 150,
+                    },
+                  },
+                ];
+              }}
+              keyAccessor={(data) => data.uid}
+            >
+              {(nodes) => {
+                return (
+                  <>
+                    {nodes.map((node, index) => {
+                      return (
+                        <div
+                          key={node.key}
+                          style={{
+                            opacity: node.state.opacity,
+                            height: node.state.y,
+                          }}
+                        >
+                          {itemListProps.renderItem(node.data, index)}
+                        </div>
+                      );
+                    })}
+                  </>
+                );
+              }}
+            </NodeGroup>
+          </Menu>
+        );
+      }}
+    />
+  );
 }
 
 function escapeRegExpChars(text: string) {
@@ -382,7 +471,7 @@ function highlightText(text: string, query: string) {
   let lastIndex = 0;
   const words = query
     .split(/\s+/)
-    .filter(word => word.length > 0)
+    .filter((word) => word.length > 0)
     .map(escapeRegExpChars);
   if (words.length === 0) {
     return [text];
@@ -409,7 +498,6 @@ function highlightText(text: string, query: string) {
   return tokens;
 }
 
-
 function useEvent(handler: Function) {
   const handlerRef = useRef(null);
 
@@ -428,8 +516,8 @@ function useEvent(handler: Function) {
 const openUid = (uid: string) => {
   window.roamAlphaAPI.ui.mainWindow.openBlock({
     block: {
-      uid: uid
-    }
+      uid: uid,
+    },
   });
 };
 
@@ -459,7 +547,7 @@ function getPageTitleByUid(uid: string) {
 `);
 }
 
-let API: RoamExtensionAPI
+let API: RoamExtensionAPI;
 
 export function initExtension(extensionAPI: RoamExtensionAPI) {
   API = extensionAPI;
@@ -470,28 +558,25 @@ export function initExtension(extensionAPI: RoamExtensionAPI) {
   }
   mount();
   extension_helper.on_uninstall(() => {
-    document.querySelector(`.${clazz}`)?.remove()
-  })
+    document.querySelector(`.${clazz}`)?.remove();
+  });
 }
-
 
 function openInSidebar(uid: string) {
   window.roamAlphaAPI.ui.rightSidebar.addWindow({
     window: {
-      'block-uid': uid,
-      type: 'outline'
-    }
-  })
+      "block-uid": uid,
+      type: "outline",
+    },
+  });
 }
 function recordPosition() {
-
   if (currentTab && currentTab.scrollTop !== scrollTop$) {
-    console.log(currentTab, '----before record----', tabs)
-    currentTab.scrollTop = scrollTop$
-    const tab = tabs.find((tab) => tab.uid === currentTab.uid)
-    if (tab)
-      tab.scrollTop = scrollTop$
+    console.log(currentTab, "----before record----", tabs);
+    currentTab.scrollTop = scrollTop$;
+    const tab = tabs.find((tab) => tab.uid === currentTab.uid);
+    if (tab) tab.scrollTop = scrollTop$;
   }
 
-  console.log(currentTab, '----after record----', tabs)
+  console.log(currentTab, "----after record----", tabs);
 }
