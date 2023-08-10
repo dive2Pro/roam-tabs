691cc0c9bed225f1be49f46ee3ea0f0d17e56dac
toyluck <toylucklebo@gmail.com>
2023-08-10 20:58:36 +0800

    feat: code format

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,44 @@
-
 const Keys = {
-    Auto: 'Auto',
-    Tabs: 'Tabs',
-}
+  Auto: "Auto",
+  Tabs: "Tabs",
+  Close: "Close",
+};
 
 let API: RoamExtensionAPI;
 export function initConfig(extensionAPI: RoamExtensionAPI) {
-    API = extensionAPI
-    extensionAPI.settings.panel.create({
-        tabTitle: 'Tabs',
-        settings: [{
-            id: Keys.Auto,
-            name: 'Auto Mode',
-            description: 'Automatically open links in new tabs',
-            action: {
-                type: "switch",
-                onChange: (evt: { target: { checked: boolean } }) => {
-                    console.log(evt, ' ----@@@')
-                    API.settings.set(Keys.Auto, evt.target.checked);
-                }
-            }
-        }]
-    })
+  API = extensionAPI;
+  extensionAPI.settings.panel.create({
+    tabTitle: "Tabs",
+    settings: [
+      {
+        id: Keys.Auto,
+        name: "Auto Mode",
+        description: "Automatically open links in new tabs",
+        action: {
+          type: "switch",
+          onChange: (evt: { target: { checked: boolean } }) => {
+            console.log(evt, " ----@@@");
+            API.settings.set(Keys.Auto, evt.target.checked);
+          },
+        },
+      },
+    ],
+  });
 }
 
 export function isAutoOpenNewTab() {
-    return API.settings.get(Keys.Auto) === true;
+  return API.settings.get(Keys.Auto) === true;
 }
 
-type CacheTab = { tabs: Tab[], activeTab: Tab };
+type CacheTab = { tabs: Tab[]; activeTab: Tab };
 
 export function loadTabsFromSettings(): CacheTab {
-    return (API.settings.get(Keys.Tabs) as CacheTab)
+  return API.settings.get(Keys.Tabs) as CacheTab;
 }
 
 export function saveTabsToSettings(tabs: Tab[], activeTab: Tab) {
-    API.settings.set(Keys.Tabs, {
-        tabs,
-        activeTab
-    })
+  API.settings.set(Keys.Tabs, {
+    tabs,
+    activeTab,
+  });
 }
